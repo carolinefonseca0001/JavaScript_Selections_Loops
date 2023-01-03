427063003d1716d08c2f1f1890453291a4cd8249
Caroline <caroline.fonseca0001@gmail.com>
2023-01-02 21:11:21 -0500

    Completed exercises 1 and 2

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
+
 console.log("EXERCISE 1:\n==========\n");
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+        continue;
+    } else {
+        console.log(i)
+    }
+}
 
 // Exercise 2 Section
+console.log("EXERCISE 2:\n==========\n");
+
+for (let a = 1; a < 100; a++){
+    if (a % 3 == 0){
+        console.log('FIZZ');
+        continue;
+}
+    if (a % 5 == 0){
+        console.log('BUZZ');
+        continue;
+} 
+    else{
+        console.log('FIZZBUZZ')
+}
+
+}
+
+
+// Exercise 3 Section
+console.log("EXERCISE 2:\n==========\n");
+
+// Exercise 4 Section
+console.log("EXERCISE 2:\n==========\n");
+
+// Exercise 5 Section
 console.log("EXERCISE 2:\n==========\n");
